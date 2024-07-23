6e9407e37c5225f0e8b8a3ccacc297f0c7e7f087
efron <eahmadifar@outlook.com>
2024-07-23 16:08:46 +1000

    chore: Update project dependencies and add CommunityToolkit.Maui package

--- Program.cs
+++ Program.cs
@@ -13,12 +13,12 @@ partial class Program
 {
     private static readonly int _led1Pin = 18;
     private static readonly int _led2Pin = 24;
-    // private static GpioController? _gpioController;
+
     private static HttpClient _httpClient = new HttpClient();
     private static SpeechSynthesizer? _speechSynthesizer;
     private static ChatGPTClient? _chatGPTClient;
     private static SpeechToTextController? _speechToTextController;
-    // private static AudioPlayer _audioPlayer;
+
     private static PicovoiceHandler? _picovoiceHandler;
     private static WhisperClient? _whisperClient;
     public static IConfiguration? Configuration { get; private set; }
@@ -38,41 +38,12 @@ partial class Program
         try
         {
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
-            // Initialize GPIO
-            // _gpioController = new GpioController();
-            // _gpioController.OpenPin(_led1Pin, PinMode.Output);
-            // _gpioController.OpenPin(_led2Pin, PinMode.Output);
-            // _gpioController.Write(_led1Pin, PinValue.Low);
-            // _gpioController.Write(_led2Pin, PinValue.Low);
-
-            // Load configuration
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) // Base settings
-            .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true) // Environment-specific settings
-            .AddEnvironmentVariables(); // Environment variables
-
-
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
             Configuration = builder.Build();
-
-            // var host = Host.CreateDefaultBuilder(args)
-            //             .ConfigureServices((context, services) =>
-            //             {
-            //                 services.AddSingleton<IConfiguration>(Configuration);
-            //                 services.AddSingleton<ConfigurationReloader>();
-            //             })
-            //             .ConfigureLogging(logging =>
-            //             {
-            //                 logging.AddConsole();
-            //             })
-            //             .Build();
-
-            // var configReloader = host.Services.GetRequiredService<ConfigurationReloader>();
-            // configReloader.StartListening();
-
-            // await host.RunAsync();
-
-
             var openaiApiKey = Configuration["OPENAI_API_KEY"] ?? "OPENAI_API_KEY";
             var gptModel = Configuration["GPT_MODEL"] ?? "gpt-3.5-turbo-16k";
             var pvAccessKey = Configuration["PV_ACCESS_KEY"] ?? "PV_ACCESS_KEY";
@@ -84,50 +55,50 @@ partial class Program
             int cheetahAudioDeviceIndex;
             if (!int.TryParse(Configuration["CHEETAH_AUDIO_DEVICE_INDEX"], out cheetahAudioDeviceIndex))
             {
-                cheetahAudioDeviceIndex = -1; // Default value if parsing fails
+                cheetahAudioDeviceIndex = -1;
             }
             var cheetahAccessKey = Configuration["CHEETAH_ACCESS_KEY"] ?? "";
             var cheetahModelPath = Configuration["CHEETAH_MODEL_PATH"] ?? "";
             float cheetahEndpointDurationSec;
             if (!float.TryParse(Configuration["CHEETAH_ENDPOINT_DURATION_SEC"], out cheetahEndpointDurationSec))
             {
-                cheetahEndpointDurationSec = 3.0f; // Default value if parsing fails
+                cheetahEndpointDurationSec = 3.0f;
             }
             bool cheetahEnableAutomaticPunctuation;
             if (!bool.TryParse(Configuration["CHEETAH_ENABLE_AUTOMATIC_PUNCTUATION"], out cheetahEnableAutomaticPunctuation))
             {
-                cheetahEnableAutomaticPunctuation = true; // Default value if parsing fails
+                cheetahEnableAutomaticPunctuation = true;
             }
-            var keywordPaths = new List<string> { "./Hey-Wiz_en_windows_v3_0_0.ppn" }; // Add your keyword paths
-            var sensitivities = new List<float> { 0.5f }; // Adjust sensitivities as needed
-            int audioDeviceIndex = -1; // Default audio device index
-            // Initialize ChatGPTClient
-            _chatGPTClient = new ChatGPTClient(baseAddress, openaiApiKey, gptModel);
+            var keywordPaths = new List<string> { "./Hey-Wiz_en_linux_v3_0_0.ppn" };
+            var sensitivities = new List<float> { 0.5f };
+            int audioDeviceIndex = -1;
 
-            // Initialize SpeechSynthesizer
-            _speechSynthesizer = new SpeechSynthesizer(awsAccessKeyId, awsSecretAccessKey, Amazon.RegionEndpoint.USEast1);
+            // _chatGPTClient = new ChatGPTClient(baseAddress, openaiApiKey, gptModel);
 
-            // Initialize PicovoiceHandler
+
+            // _speechSynthesizer = new SpeechSynthesizer(awsAccessKeyId, awsSecretAccessKey, Amazon.RegionEndpoint.USEast1);
+
+
             _picovoiceHandler = new PicovoiceHandler(pvAccessKey, _led1Pin, _led2Pin, keywordPaths, sensitivities, audioDeviceIndex);
             _picovoiceHandler.WakeWordDetected += OnWakeWordDetected;
             _picovoiceHandler.Start();
 
-            // Initialize Recorder
-            _speechToTextController = new SpeechToTextController(cheetahAccessKey, cheetahModelPath, cheetahEndpointDurationSec, cheetahEnableAutomaticPunctuation, 1);
-            // _audioPlayer = new AudioPlayer();
-            _whisperClient = new WhisperClient(whisperApiUrl, openaiApiKey, whisperModel);
-            // Main loop to keep the application running
+
+            // _speechToTextController = new SpeechToTextController();
+
+            // _whisperClient = new WhisperClient(whisperApiUrl, openaiApiKey, whisperModel);
+
             Console.WriteLine("Application started. Press Ctrl+C to exit.");
             while (true)
             {
-                await Task.Delay(1000); // Keeping the main thread alive
+                await Task.Delay(1000);
             }
         }
         catch (Exception ex)
         {
-            // Log the exception and exit gracefully
+
             Console.WriteLine($"An error occurred: {ex.Message}");
-            // Optionally, log to a file or logging service
+
         }
     }
 
@@ -136,53 +107,38 @@ partial class Program
         Console.WriteLine("Wake word detected, listening for speech...");
         try
         {
-            // Simulate waiting for the speech capture
-            await Task.Delay(5000); // Replace with actual speech capture logic
 
-            // Use a random prompt from the array to interact with the user
+            await Task.Delay(5000);
+
+
             var random = new Random();
             string prompt = _prompts[random.Next(_prompts.Length)];
             Console.WriteLine($"Prompt: {prompt}");
 
-            // Synthesize the prompt to speech
+
             if (_speechSynthesizer != null)
                 await _speechSynthesizer.SynthesizeSpeechAsync(prompt);
 
-            // Converting speech to text
-            string? transcript = _speechToTextController?.SpeechToText();
 
-            // Simulate listening for voice commands
-            // await Task.Delay(5000); // Replace with actual listening logic
+            string? transcript = await _speechToTextController?.SpeechToTextAsync(CancellationToken.None);
 
-            // Wait for silence to detect end of speech
-            // if (_speechToText != null)
-            //     await _speechToText.StopIfSilenceDetected();
-            // _recorder.Stop();
-
-            // Process the recording
-            // short[] pcmData = _speechToTextController?.GetPcmData() ?? throw new Exception("Recorder is null.");
-            // string transcript = _whisperClient != null ? await _whisperClient.ConvertPcmToTextAsync(pcmData) : throw new Exception("Whisper client is null."); // This should be replaced with actual speech-to-text result
-
-
-
-            // Get a response from ChatGPT
             string? gptResponse = null;
             if (transcript != null)
                 gptResponse = _chatGPTClient != null ? await _chatGPTClient.GetResponseAsync(transcript) : throw new Exception("ChatGPTClient is null.");
             else throw new Exception("Transcript is null.");
 
-            // Play the response
-            // _audioPlayer.PlayMp3("speech.mp3");
+
+
             if (_speechSynthesizer != null && gptResponse != null)
                 await _speechSynthesizer.SynthesizeSpeechAsync(gptResponse);
             else
                 throw new Exception("SpeechSynthesizer or GPT response is null.");
 
-            // Fade LEDs
-            // await FadeLedsAsync(fadeIn: false);
+
+
             Console.WriteLine($"Response has been spoken: {gptResponse}");
 
-            // Synthesize the response to speech
+
             if (_speechSynthesizer != null && gptResponse != null)
                 await _speechSynthesizer.SynthesizeSpeechAsync(gptResponse);
             else
@@ -190,41 +146,35 @@ partial class Program
 
             Console.WriteLine("Response has been spoken.");
 
-            // Reset GPIO
-            // if (_gpioController != null)
-            // {
-            //     _gpioController.Write(_led1Pin, PinValue.Low);
-            //     _gpioController.Write(_led2Pin, PinValue.Low);
-            // }
         }
         catch (HttpRequestException httpEx)
         {
             Console.WriteLine($"HTTP error occurred: {httpEx.Message}");
-            // Handle HTTP request exceptions
+
         }
         catch (TaskCanceledException taskEx)
         {
             Console.WriteLine($"Task was canceled: {taskEx.Message}");
-            // Handle task cancellation exceptions
+
         }
         catch (Exception ex)
         {
             Console.WriteLine($"An error occurred: {ex.Message}");
-            // Handle other exceptions
+
         }
     }
 
-    // private static async Task FadeLedsAsync(bool fadeIn = true)
-    // {
-    //     try
-    //     {
-    //         using var pwmController = _gpioController != null ? new PwmController(_gpioController, _led1Pin) : throw new Exception("_gpioController is null.");
-    //         await pwmController.FadeAsync(2000, fadeIn); // Fade duration in milliseconds
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         Console.WriteLine($"An error occurred while fading LEDs: {ex.Message}");
-    //         // Handle exceptions related to PWM control
-    //     }
-    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- SpeechToTextController.cs
+++ SpeechToTextController.cs
@@ -1,146 +1,44 @@
-// using NAudio.Wave;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using Pv;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Media;
 
 public class SpeechToTextController
 {
-    private readonly int _silenceThreshold; // Threshold for detecting silence
-    private readonly int _silenceDuration; // Duration of silence to detect before stopping
-    private bool _isRecording;
-    // private WaveInEvent _waveIn;
-    private MemoryStream _memoryStream;
-    private List<byte> _audioBuffer;
-    private static string? _accessKey;
-    private static string? _modelPath;
-    private static int? _audioDeviceIndex;
-    private static bool _enableAutomaticPunctuation;
-    private static float _endpointDurationSec;
-    private const int SilenceThreshold = 100;
+    private readonly ISpeechToText _speechToText;
+    private string RecognitionText;
 
-    public SpeechToTextController(string accessKey,
-            string modelPath,
-            float endpointDurationSec,
-            bool enableAutomaticPunctuation,
-            int? audioDeviceIndex)
+    public SpeechToTextController()
     {
-        _accessKey = accessKey;
-        _modelPath = modelPath;
-        _endpointDurationSec = endpointDurationSec;
-        _enableAutomaticPunctuation = enableAutomaticPunctuation;
-        _audioDeviceIndex = audioDeviceIndex;
-
-
-        // _waveIn = new WaveInEvent();
-        // _waveIn.WaveFormat = new WaveFormat(16000, 1); // 16kHz, Mono
-        // _waveIn.DataAvailable += OnDataAvailable;
-        // _waveIn.RecordingStopped += OnRecordingStopped;
-
-        _memoryStream = new MemoryStream();
-        _audioBuffer = new List<byte>();
-
+        _speechToText = SpeechToText.Default;
     }
 
-    public string? SpeechToText()
+    public async Task<string> SpeechToTextAsync(CancellationToken cancellationToken)
     {
-        // Start recording logic
-        // _audioBuffer.Clear();
-        // _waveIn.StartRecording();
-        using (Cheetah cheetah = Cheetah.Create(
-                accessKey: _accessKey,
-                modelPath: _modelPath,
-                endpointDurationSec: _endpointDurationSec,
-                enableAutomaticPunctuation: _enableAutomaticPunctuation))
+        var isGranted = await _speechToText.RequestPermissions(cancellationToken);
+        if (!isGranted)
         {
-
-            // create recorder
-            using (PvRecorder recorder = PvRecorder.Create(cheetah.FrameLength, _audioDeviceIndex.Value))
-            {
-                Console.WriteLine($"Using device: {recorder.SelectedDevice}");
-                Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e)
-                {
-                    e.Cancel = true;
-                    recorder.Stop();
-                    Console.WriteLine("Stopping...");
-                };
-                var transcript = "";
-
-                recorder.Start();
-                Console.WriteLine(">>> Press `CTRL+C` to exit:\n");
-
-                try
-                {
-                    while (recorder.IsRecording)
-                    {
-                        short[] frame = recorder.Read();
-
-                        CheetahTranscript result = cheetah.Process(frame);
-                        if (!string.IsNullOrEmpty(result.Transcript))
-                        {
-                            Console.Write(result.Transcript);
-                        }
-                        if (result.IsEndpoint)
-                        {
-                            CheetahTranscript finalTranscriptObj = cheetah.Flush();
-                            Console.WriteLine(finalTranscriptObj.Transcript);
-                            transcript += finalTranscriptObj.Transcript;
-                            recorder.Stop();
-                            break;
-                        }
-
-                    }
-                }
-                catch (CheetahActivationLimitException)
-                {
-                    Console.WriteLine($"AccessKey '{_accessKey}' has reached its processing limit.");
-                }
-                return transcript;
-            }
+            await Toast.Make("Permission not granted").Show(CancellationToken.None);
+            return string.Empty;
         }
+
+        var recognitionResult = await _speechToText.ListenAsync(
+            CultureInfo.GetCultureInfo("en-US"),
+            new Progress<string>(partialText =>
+            {
+                RecognitionText += partialText + " ";
+            }), cancellationToken);
+
+        if (recognitionResult.IsSuccessful)
+        {
+            RecognitionText = recognitionResult.Text;
+        }
+        else
+        {
+            await Toast.Make(recognitionResult.Exception?.Message ?? "Unable to recognize speech").Show(CancellationToken.None);
+        }
+
+        return RecognitionText;
     }
-
-    // public void Stop()
-    // {
-    //     // Stop recording logic
-    //     _waveIn.StopRecording();
-    // }
-    // private void OnDataAvailable(object sender, WaveInEventArgs e)
-    // {
-    //     _audioBuffer.AddRange(e.Buffer);
-    // }
-
-    // private void OnRecordingStopped(object sender, StoppedEventArgs e)
-    // {
-    //     _memoryStream.Write(_audioBuffer.ToArray(), 0, _audioBuffer.Count);
-    // }
-
-
-    // public bool IsSilent()
-    // {
-    //     short[] pcmData = GetPcmData();
-    //     foreach (var sample in pcmData)
-    //     {
-    //         if (Math.Abs(sample) > SilenceThreshold)
-    //         {
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-    // public async Task StopIfSilenceDetected()
-    // {
-    //     while (!IsSilent())
-    //     {
-    //         await Task.Delay(100); // Check every 100ms for silence
-    //     }
-    //     Stop();
-    // }
-    // public short[] GetPcmData()
-    // {
-    //     byte[] byteData = _audioBuffer.ToArray();
-    //     short[] pcmData = new short[byteData.Length / sizeof(short)];
-    //     Buffer.BlockCopy(byteData, 0, pcmData, 0, byteData.Length);
-    //     return pcmData;
-    // }
 }
